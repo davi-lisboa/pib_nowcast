85dc3a2c967a2d891dcbfa352f43ef30f856970a
Davi Lisboa <lisboa.davi1703@gmail.com>
2025-06-25 23:27:17 -0300

    Create dashboard_creation.py

---
+++ dashboard_creation.py
@@ -0,0 +1,120 @@
+# %% Bibliotecas
+import pandas as pd
+import numpy as np
+import datetime as dt
+from datetime import timedelta
+
+from bcb import sgs
+import sidrapy as sidra
+
+# import statsmodels.api as sm
+from statsmodels.tsa.statespace.dynamic_factor_mq import DynamicFactorMQ
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+# from sktime.utils.plotting import plot_series
+
+import streamlit as st
+
+import pickle
+import gzip
+
+import warnings
+warnings.filterwarnings("ignore")
+# %% Importando dataset e modelo treinado
+
+@st.cache_data
+def load_dataset():
+    with gzip.open("initial_dataset.pkl.gz", "rb") as f:
+        return  pickle.load(f)
+
+@st.cache_data
+def load_model():
+    with gzip.open("initial_model.pkl.gz", "rb") as f:
+        return pickle.load(f)
+
+df_completo = load_dataset()
+dfmq = load_model()
+
+pib = df_completo[['pib']].resample('QE').last()
+mensais = df_completo.drop(columns=['pib'])
+
+# %% Nowcasts
+
+last_month_available = df_completo.index.max()
+steps = 15 - df_completo.index.max().month
+future_month = last_month_available + pd.offsets.MonthEnd(steps)
+nowcast_obj = dfmq.get_prediction( end=future_month)
+
+nowcast = nowcast_obj.predicted_mean['pib'].rename('pib_nowcast').to_frame()
+
+nowcast = nowcast.assign(
+    lower_pib=nowcast_obj.conf_int().loc[:, 'lower pib'],
+    upper_pib=nowcast_obj.conf_int().loc[:, 'upper pib']
+)
+nowcast.index = nowcast.index.to_timestamp()
+nowcast = nowcast.resample('QE').last().round(1)
+# nowcast_confint = nowcast_obj.conf_int().loc[:, ['lower pib', 'upper pib']]
+
+df_graficos = pib.merge(nowcast, 
+                        how='outer', 
+                        left_index=True, 
+                        right_index=True).round(1)
+df_graficos
+
+# %% Criação dash streamlit
+
+st.title('Nowcast do PIB Real')
+st.write('Este aplicativo apresenta o nowcast do PIB Real brasileiro, calculado com base em um modelo de fatores dinâmicos mensais (DFMQ).')
+
+st.sidebar.header('Ajustes do Gráfico')
+
+data_inicio = (pib.index.max() - pd.offsets.QuarterEnd(40)).to_pydatetime()
+data_fim = df_graficos.index.max().to_pydatetime()#.strftime('%Y-%m-%d')
+intervalo_data = st.sidebar.slider(
+                            "Filtre o período",
+                            min_value=data_inicio,
+                            max_value=data_fim,
+                            value=(data_inicio, data_fim),
+                            # step=timedelta(days=30 )
+                            )
+
+df_graficos = df_graficos.loc[intervalo_data[0]:intervalo_data[1], :]
+
+st.plotly_chart(px.line(
+                            df_graficos,
+                            x=df_graficos.index,
+                            y=['pib', 'pib_nowcast', ],
+                            title='Nowcast do PIB Real',
+                            labels={'value': 'YoY%', 'index':''},
+                            markers=True,
+                            color_discrete_sequence=["#288BC5", 'orange'],
+                                
+                        ) \
+                        .update_traces(name='PIB Real YoY%', selector=dict(name='pib')) \
+                        .update_traces(name='Nowcast PIB YoY%', selector=dict(name='pib_nowcast')) \
+                        .add_scatter(
+                                        x=df_graficos.index,
+                                        y=df_graficos['lower_pib'],
+                                        mode='lines',
+                                        line=dict(color='lightgray', width=1, dash='dash'),
+                                        # fill='tonexty',
+                                        # fillcolor='rgba(211, 211, 211, 0.4)',
+                                        showlegend=False
+                                    ) \
+                        .add_scatter(
+                                        x=df_graficos.index,
+                                        y=df_graficos['upper_pib'],
+                                        mode='lines',
+                                        line=dict(color='lightgray', width=1, dash='dash'),
+                                        fill='tonexty',
+                                        # fillcolor='rgba(211, 211, 211, 0.4)',
+                                        showlegend=False
+                                    )   
+)
+
+st.write('### Resumo do Modelo')
+st.write(dfmq.summary())
+
+# %%
